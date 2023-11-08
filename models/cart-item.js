9a988572a7c12a84354176b3ae5fa9e0e2476e17
RoshanPotale <rpotale0107@gmail.com>
2023-11-08 16:00:21 +0530

    code

---
+++ models/cart-item.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const cartItemSchema = mongoose.Schema({
+    quantity: {
+        type: Number,
+        required: true
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    }
+});
+exports.CartItem = mongoose.model('CartItem', cartItemSchema);
